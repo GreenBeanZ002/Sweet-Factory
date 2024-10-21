d31d6be0c88a3c50a1a6957538462b8d7abd7c7e
GreenBeanZ002 <110616479+GreenBeanZ002@users.noreply.github.com>
2024-10-21 12:28:11 +0100

    Added slight camera shift when sweet is dropped

--- scripts/sweetTransform.cs
+++ scripts/sweetTransform.cs
@@ -6,6 +6,8 @@ public class sweetTransform : MonoBehaviour
 {
     public GameObject originalSweet;
     public GameObject newSweet;
+    public Camera camera;
+
     void Update()
     {
         //When the "Space Key is pressed spawns a new sweet prefab and deletes the static sweet
@@ -13,6 +15,7 @@ public class sweetTransform : MonoBehaviour
         {
             var transformedSweet = Instantiate(newSweet, new Vector3(originalSweet.transform.position.x, originalSweet.transform.position.y, 0), Quaternion.identity);
             Destroy(originalSweet);
+            camera.transform.Translate(0, -3, 0);
         }
     }
 }
